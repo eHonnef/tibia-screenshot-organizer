337daf8906521775ea1c684e269ff058d1ed67ca
honnef <honnef>
2021-04-22 14:55:13 -0300

    first

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# tibia-screenshot-organizer
-Script to organize the tibia's screenshot folder.
+# Tibia screenshot organizer
+
+Script to organize the tibia's screenshot folder. It'll move (or copy) the screenshots from the tibia's screenshot folder to another folder and organize by `character name and screenshot type` (e.g.: LevelUP, DeathPVE, Hotkey, etc).
+
+## Requirements
+
+Only Python 3+.
+
+## Usage
+
+Change the values in the `settings.json` file:
+
+- print-dir: Put the full path to tibia's screenshot folder, usually `C:/Program files/Tibia/packages/Tibia/screenshots`.
+- destination: The destination folder that'll have all the organized screenshots.
+- copy: `True` - It'll keep the screenshots in tibia folder. `False` - It'll remove the screenshots from tibia folder.
+
+**It's recomended to do a backup of tibia's screenshot folder before using this script.**
+
+## License
+
+Licensed under MIT license, check the [License](LICENSE) file for more information.
---
+++ tibia_print_organizer.py
@@ -0,0 +1,50 @@
+import shutil
+import os
+import json
+
+# Configuration file
+config_file = "./settings.json"
+# Tibia screenshot dir
+tibia_print_dir = "C:/etc/Tibia/packages/Tibia/screenshots"
+# Destination dir of the screenshots
+dest = "./Prints"
+# Move screenshots or copy
+copy = False
+# Default operation
+operation = shutil.move
+
+if os.path.exists(config_file):
+  j = json.load(open(config_file, "r"))
+  tibia_print_dir = j.get("print-dir", tibia_print_dir)
+  dest = j.get("destination", dest)
+  copy = j.get("copy", copy)
+
+if copy:
+  operation = shutil.copy
+
+# Create the destination dir if doesn't exist
+if not os.path.exists(dest):
+  os.makedirs(dest)
+
+# List screenshots in tibia's dir
+prints = [f.path for f in os.scandir(tibia_print_dir) if not f.is_dir()]
+
+for pr in prints:
+  prs = pr.split("_")
+  # Get the char dir
+  char_dir = dest + "/" + prs[-2]
+  # Get the print type (death, levelUP, etc)
+  sstype_dir = dest + "/" + prs[-2] + "/" + prs[-1].split(".")[0]
+
+  # Create a dir per char
+  if not os.path.exists(char_dir):
+    os.makedirs(char_dir)
+
+  # Create a dir per type of screenshot
+  if not os.path.exists(sstype_dir):
+    os.makedirs(sstype_dir)
+
+  # Move files to destination
+  operation(pr, sstype_dir)
+
+print("DONE!")
